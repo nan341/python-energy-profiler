39dbdeb14b180f2b72d90db990c413b1027e5c37
Nandini Madhur <nandinimadhur@gmail.com>
2026-05-05 14:06:43 +0530

    CPU usage estimation using psutil

--- profiler/profiler.py
+++ profiler/profiler.py
@@ -1,17 +1,27 @@
 import time
+import psutil
 
 
 def profile_time(func):
     def wrapper(*args, **kwargs):
+        process = psutil.Process()
+
+        process.cpu_percent(interval=None)  # warm-up
+
         start_time = time.perf_counter()
+        cpu_before = process.cpu_percent(interval=None)
 
         result = func(*args, **kwargs)
 
+        cpu_after = process.cpu_percent(interval=None)
         end_time = time.perf_counter()
 
         execution_time = end_time - start_time
+        avg_cpu = (cpu_before + cpu_after) / 2
 
-        print(f"{func.__name__} executed in {execution_time:.6f} seconds")
+        print(f"{func.__name__}:")
+        print(f"  Time: {execution_time:.6f} sec")
+        print(f"  CPU (approx): {avg_cpu:.2f}%")
 
         return result
 
